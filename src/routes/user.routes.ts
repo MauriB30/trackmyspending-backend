6855f93b976267f461055565a926d521baeb55f2
Mauricio Blanco <mauriblanco29@gmail.com>
2025-10-14 17:07:42 -0300

    chore: remove import of non-existent file

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,10 +1,9 @@
 import { Router } from 'express';
 import { getUser } from '../controllers/user/getUser';
 import { authenticateUser } from '../middleware/authMiddlewares';
-import { verifySiteOrigin } from '../middleware/verifySiteOrigin';
 
 const userRoutes = Router();
 
-userRoutes.get('/me', verifySiteOrigin, authenticateUser, getUser);
+userRoutes.get('/me', authenticateUser, getUser);
 
 export default userRoutes;
